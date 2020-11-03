4915df71535271d1d352aee4cfa48dd685088d2a
unknown <howell.kenneth.a@gmail.com>
2020-11-03 12:37:28 -0600

    encapsulated jquery in sep .js to avoid distraction, set up commented structure of code to go over during lecture from own examples / curri / slideshow @ TDR in chronological order of presentation

---
+++ htmlJS.js
@@ -0,0 +1,68 @@
+
+$("#one").hover(function () {
+    $("#showOne").toggle(300);
+})
+
+$("#two").hover(function () {
+    $("#showTwo").toggle(300);
+})
+
+$("#three").hover(function () {
+    $("#showThree").toggle(300);
+})
+
+$("#four").hover(function () {
+    $("#showFour").toggle(300);
+})
+
+$("#five").hover(function () {
+    $("#showFive").toggle(300);
+})
+
+// function popovers
+
+$("#functionOne").click(function () {
+    $("#functionPopover1").toggle(300);
+})
+
+$("#functionTwo").click(function () {
+    $("#functionPopover2").toggle(300);
+})
+
+$("#functionThree").click(function () {
+    $("#functionPopover3").toggle(300);
+})
+
+$("#functionFour").click(function () {
+    $("#functionPopover4").toggle(300);
+})
+
+
+$("#functionSimplerOne").click(function () {
+    $("#functionSimplerPopover1").toggle(300);
+})
+
+$("#functionSimplerTwo").click(function () {
+    $("#functionSimplerPopover2").toggle(300);
+})
+
+$("#functionSimplerThree").click(function () {
+    $("#functionSimplerPopover3").toggle(300);
+})
+
+$("#functionSimplerFour").click(function () {
+    $("#functionSimplerPopover4").toggle(300);
+})
+
+$("#tilde").click(function () {
+    $("#functionTopExCont").toggle(100);
+    $("#functionBottomExCont").toggle(100);
+    $("#functionExCodeCont").toggle("slow");
+
+})
+
+$("#penUltTilde").click(function () {
+    $("#functionTopExCont").toggle(100);
+    $("#functionBottomExCont").toggle(100);
+    $("#functionExCodeCont2").toggle("slow");
+})
