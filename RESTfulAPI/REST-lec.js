e8398dcffa6b4c1a9c983e8f647c6bd0c7f32181
unknown <howell.kenneth.a@gmail.com>
2021-01-13 19:28:52 -0600

    built out fetch GET request and fetch POST request pointing to glitch mentioned in the url > sends json post of object up to glitch

---
+++ RESTfulAPI/REST-lec.js
@@ -0,0 +1,47 @@
+// ~~ ** REST requests / responses ** ~~
+//
+// const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
+// const url = '/posts';
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(blogPost),
+// };
+// fetch(url, options)
+//     .then(/* post was created successfully */)
+//     .catch(/* handle errors */);
+
+// console.log(options);
+
+
+// https://watery-hammerhead-bay.glitch.me/ :: Hello Express remix link
+// /dreams = our route
+
+const dreamApiURL = "https://watery-hammerhead-bay.glitch.me/dreams";
+
+
+// fetch all of the dreams
+// fetch(dreamApiURL).then(function (response) {
+//    response.json().then(function(dreams){
+//     console.log(dreams);
+//
+// })
+//     })
+
+const dreamObj = {
+    "body": "I love my girlfriend"
+};
+
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dreamObj),
+};
+
+fetch(dreamApiURL, options).then(function (response){
+    console.log(response);
+})
