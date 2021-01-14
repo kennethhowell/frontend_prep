9c411cc5e43db1f47e37530d428af8ebc5306467
unknown <howell.kenneth.a@gmail.com>
2021-01-14 12:45:25 -0600

    expanded and fleshed out with different methods - found good resource too, linked up top

--- RESTfulAPI/REST-lec.js
+++ RESTfulAPI/REST-lec.js
@@ -1,4 +1,5 @@
 // ~~ ** REST requests / responses ** ~~
+// good reference material: https://medium.com/@9cv9official/what-are-get-post-put-patch-delete-a-walkthrough-with-javascripts-fetch-api-17be31755d28
 //
 // const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
 // const url = '/posts';
@@ -16,7 +17,7 @@
 // console.log(options);
 
 
-// https://watery-hammerhead-bay.glitch.me/ :: Hello Express remix link
+// https://watery-hammerhead-bay.glitch.me/ :: our base URL for our API
 // /dreams = our route
 
 const dreamApiURL = "https://watery-hammerhead-bay.glitch.me/dreams";
@@ -30,10 +31,14 @@ const dreamApiURL = "https://watery-hammerhead-bay.glitch.me/dreams";
 // })
 //     })
 
+//Let's start manipulating some data!
+
 // const dreamObj = {
 //     "dream": "My dream is to have another kitty"
 // };
-//
+
+// Add a new dream [dreamObj] up to our page that is brand spanking new
+
 // const options = {
 //     method: 'POST',
 //     headers: {
@@ -41,7 +46,34 @@ const dreamApiURL = "https://watery-hammerhead-bay.glitch.me/dreams";
 //     },
 //     body: JSON.stringify(dreamObj),
 // };
+
+// Target a specific url and update that with an entirely new dream (an {entire: object})
+
+// const options = {
+//     method: 'PUT',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(dreamObj),
+// };
+
+
+// Target a specific url and update a specific property within ({property: changes})
+// const options = {
+//     method: 'PATCH',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({dream: "Patch up this dream"})
+// };
+
+//Target a specific url and delete (permanently - no take backs) that target
+
+// const options = {
+//     method: 'DELETE',
+//     }
+
 //
-// fetch(dreamApiURL, options).then(function (response){
-//     console.log(response);
-// })
+fetch(dreamApiURL + "/3", options).then(function (response){
+    console.log(response);
+})
