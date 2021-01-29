14697e3afb2a16cdd1486243fb7ba0f02ba3461a
kennethhowell <howell.kenneth.a@gmail.com>
2021-01-29 12:36:27 -0600

    refreshed material mentally - decluttered scope example - running functions in console to be dbl sure everything WAI

--- functions-lec.js
+++ functions-lec.js
@@ -3,29 +3,34 @@
 // (function () {
 // "use strict";
 //     function someFunctionNoParameters(){
-//         return alert("Hello there, Mars && Cobol!");
+//         return alert("Hello there, Marco!");
 //     }
 //
 //     function someFunctionWithParameter(name){
 //         return alert("Hello there," + name);
 //     }
 
-// storing your return as a value
+// A function that returns a value and assign that value to a variable
 
-// for (.., .., ..) {
-// }
-
-// function increment(x) {
-//     return x + 1;
+// function increment(num) {
+//     return num + 1;
 // }
 //
 // var four = increment(3);
 // var five = increment(increment(3));
 // var six = increment(increment(increment(3)));
 
+// Or: that return can be passed into another function
+
+// function multiplyByTwo(num){
+//     return num * 2;
+// }
+
+// multiplyByTwo(increment(3))
+
 // Anonymous Functions [when passed into parent function AKA 'callback' functions]
 
-// console.log(honkHorn());
+// console.log(honkHorn()); //hoisting example - up here, what happens when calling a FUNCTION EXPRESSION vs FUNCTION DECLARATION?
 
 // function honkHorn(){
 //     return "beep beep is working";
@@ -33,48 +38,42 @@
 //
 
 // var honkHorn = function() {
-//     return "beep beep is working for sure!";
+//     return "beep beep";
 // }
 //
 // console.log(honkHorn());
 
-// a function with no return value
+// Without a return: we can execute this action - the return type defaults to undefined
 // function shout(message) {
 //      alert(message.toUpperCase() + "!!!");
-//      return message;
 // }
 //
 // var returnValue = shout('hello there');
-// console.log(returnValue); // "hello there"
+// console.log(returnValue); // undefined
 
 
-// default parameter
+// Setting a default parameter
 
-// function sayHello(name = "Casey"){
-//     return alert("Hello there," + name);
+// function sayHello(name = "Douglas"){
+//     return alert("Hello there," + name); //With no argument passed we get "Douglas"
 // }
 
 //scope
 
-// var globalVar = "Look, I'm Global!"; // declare a global variable
-//
-// function scopeExample() {
-//     // var globalVar = "Look, I'm Local!"; // declare a local variable
-//     var localVar = "Look, I'm local!";
-//     // alert(localVar); // localVar is accessible here
-//     // alert(globalVar); // globalVar is accessible here
-//     return localVar;
-// }
-//
-//
-// alert(globalVar);
-// // scopeExample();
-// alert(scopeExample());
+var globalVar = "globalVar: Look, I'm Global!"; // declare a global variable
 
-// alert(globalVar);
-// scopeExample();
+function scopeExample() {
+    var localVar = "localVar: Look, I'm local!";
+    // var globalVar = "function globalVar: I'm actually a local variable with the same name :)"
+    alert(localVar); // localVar is accessible here
+    alert(globalVar); // globalVar is accessible here
+    return "Scope example fired!";
+}
 
-// alert(localVar); // localVar is NOT accessible here and this will produce an error
+
+// alert(globalVar); // globalVar is accessible here
+// alert(localVar); // localVar is not accessible here > check the console to find an error!
+
 
 //IIFE : a wall for your JS
 
@@ -83,7 +82,7 @@
 //         var globalVar = "Now locked behind an IFFE!";
 //     console.log("Console Log: " + globalVar);
 //
-//         function sayHello(name = "Casey"){
+//         function sayHello(name = "Douglas"){
 //             return alert("Hello there," + name);
 //         }
 //
