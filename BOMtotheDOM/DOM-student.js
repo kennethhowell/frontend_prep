419fd7e850eb380b342cb03cf69f6f8c55bcf978
kennethhowell <howell.kenneth.a@gmail.com>
2021-03-28 11:31:15 -0500

    first set of activities complete with two bonuses for more challenge/early finishers

---
+++ BOMtotheDOM/DOM-student.js
@@ -0,0 +1,22 @@
+//i. locating in the DOM
+
+// TODO: Find an element by the id of "hat-search". Store the result into a variable. Use a console log to check what is in your variable.
+
+// TODO: Find all of our elements we made flex containers in Bootstrap! Locate the .d-flex class, store all of your results into a variable, and then console.log them.
+
+// TODO: Finish my FOR loop to go through that variable containing our d-flex'ed items and console.log each individual one within the loop!
+
+// for(var ; i < .length; ++){
+// .. need to console.log some stuff!}
+
+// TODO: Find all of our div elements and store them into a variable~! Console.log the variable to see the results within!
+
+// TODO: Now that we have a variable with all of the elements, can you tell me how many divs are in the HTML using that variable and a little Javascript? Hint: the variable looks and behaves a lot like an array, doesn't it? That helped us in our for loop's condition!
+
+
+// TODO BONUS (a): Write a function called locateDivs. When locateDivs is called, it should return a collection of the div elements found.
+
+// TODO BONUS (b): Write a function called locateElements. locateElements should take in one parameter, the element to be searched for, and then return a collection of those elements. If no elements by that name were found, return false.
+
+// ii. a step further: accessing / looking into what we've located deeper
+
