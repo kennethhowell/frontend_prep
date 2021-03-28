c3c2c91ca5f11b1287a6dd4d34198dc32ba0aeaf
kennethhowell <howell.kenneth.a@gmail.com>
2021-03-28 11:03:59 -0500

    NICE! The lecture code is still hooked up well from it being a refactor - adding activities to change overall structure, didn't have to reinvent the wheel

--- BOMtotheDOM/DOM-lec.js
+++ BOMtotheDOM/DOM-lec.js
@@ -34,65 +34,65 @@ for (let i = 0; i < topSellers.length; i++){
 //Accessing innerHTML
 
 //Instead of printing a list of nodes, let's print some text - What HTML is INSIDE our <li>?
-for (let i = 0; i < topSellers.length; i++){
-    console.log(topSellers[i].innerHTML);
-}
+// for (let i = 0; i < topSellers.length; i++){
+//     console.log(topSellers[i].innerHTML);
+// }
 
 //Modifying innerHTML
 //Uh oh - these were last month's top seller, WOOPS -
 //let's update our <li> to show "Hat TBD" and let's be STRONG about it!
-for (let i = 0; i < topSellers.length; i++){
-    topSellers[i].innerHTML = "<strong>Hat TBD</strong>";
-}
+// for (let i = 0; i < topSellers.length; i++){
+//     topSellers[i].innerHTML = "<strong>Hat TBD</strong>";
+// }
 
 //innerText vs innerHTML
 //"Our Top Sellers" seems odd now as well - let's adjust!
-var bestSellerHeader = document.getElementById("best-seller");
-
-bestSellerHeader.innerText = "Will update soon!"
+// var bestSellerHeader = document.getElementById("best-seller");
+//
+// bestSellerHeader.innerText = "Will update soon!"
 
 //Perhaps a little EMPHASIS will assist - will innerText help?
-bestSellerHeader.innerHTML = "<em>Will update soon!</em>"
+// bestSellerHeader.innerHTML = "<em>Will update soon!</em>"
 
 
 //Attribute methods: hasAttribute, getAttribute, setAttribute, removeAttribute
 
-//hasAttribute: return a BOOLEAN if the element has this attribute
-var bottomLink = document.getElementById("bottom-link");
-
-console.log(bottomLink.hasAttribute("href")); //true
-console.log(bottomLink.hasAttribute("style")); //false
-
-//getAttribute: what's that attribute's value? [the right side of the equation]
-console.log(bottomLink.getAttribute("href"));; //"https://www.google.com"
-
-for (let i = 0; i < topSellers.length; i++){
-    console.log(topSellers[i].getAttribute("class")); //no nada so we get 3 nulls
-}
-
-//setAttribute: let's change that attribute's value!
-bottomLink.setAttribute("href","https://www.duckduckgo.com") //Our bottom link's now headed somewhere different
-
-var searchValue = document.getElementById("searchHats"); //Let's change the value of this to something less demanding
-
-searchValue.setAttribute("value", "Find local hats!"); //That sounds better! I think I actually want it to be a PLACEHOLDER though. . :/
-
-searchValue.removeAttribute("value");
-
-searchValue.setAttribute("placeholder", "Find hats in your area") //Excellent! That's the ticket!
-
-//Accessing or Modifying styles
-
-// demonstrate the BODY as a thing to manipulate - the BODY can be targeted as any other thing:
-
-// Get the body element (notice we need to use index 0 of the array!)
-var bodyElement = document.getElementsByTagName('body')[0];
-
-// Change the body font color
-bodyElement.style.color = "rebeccapurple"; // Dark grey
-
-bodyElement.style['background-color'] = "antiquewhite";
-// We had to use [] syntax since the property name has a dash
-
-bodyElement.style.fontFamily = "Helvetica, Verdana, Sans-Serif";
-// Replace dashes with camelCase to use standard . syntax
+// //hasAttribute: return a BOOLEAN if the element has this attribute
+// var bottomLink = document.getElementById("bottom-link");
+//
+// console.log(bottomLink.hasAttribute("href")); //true
+// console.log(bottomLink.hasAttribute("style")); //false
+//
+// //getAttribute: what's that attribute's value? [the right side of the equation]
+// console.log(bottomLink.getAttribute("href"));; //"https://www.google.com"
+//
+// for (let i = 0; i < topSellers.length; i++){
+//     console.log(topSellers[i].getAttribute("class")); //no nada so we get 3 nulls
+// }
+//
+// //setAttribute: let's change that attribute's value!
+// bottomLink.setAttribute("href","https://www.duckduckgo.com") //Our bottom link's now headed somewhere different
+//
+// var searchValue = document.getElementById("searchHats"); //Let's change the value of this to something less demanding
+//
+// searchValue.setAttribute("value", "Find local hats!"); //That sounds better! I think I actually want it to be a PLACEHOLDER though. . :/
+//
+// searchValue.removeAttribute("value");
+//
+// searchValue.setAttribute("placeholder", "Find hats in your area") //Excellent! That's the ticket!
+//
+// //Accessing or Modifying styles
+//
+// // demonstrate the BODY as a thing to manipulate - the BODY can be targeted as any other thing:
+//
+// // Get the body element (notice we need to use index 0 of the array!)
+// var bodyElement = document.getElementsByTagName('body')[0];
+//
+// // Change the body font color
+// bodyElement.style.color = "rebeccapurple"; // Dark grey
+//
+// bodyElement.style['background-color'] = "antiquewhite";
+// // We had to use [] syntax since the property name has a dash
+//
+// bodyElement.style.fontFamily = "Helvetica, Verdana, Sans-Serif";
+// // Replace dashes with camelCase to use standard . syntax
