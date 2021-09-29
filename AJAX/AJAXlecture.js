6d3a793869a6e7e267664956bd6e7f47d3e301d5
kennethhowell <howell.kenneth.a@gmail.com>
2021-09-29 12:18:59 -0500

    ajax lecture retool - cat api MIA, swiched to themealdb and altered example structure.

--- AJAX/AJAXlecture.js
+++ AJAX/AJAXlecture.js
@@ -24,29 +24,29 @@ var obj = {
 // .ajax :: our first API request
 
 // // .ajax(url) - without any OPTIONS, defaults to a GET request:
-// $.ajax("https://api.spoonacular.com/recipes/random?apiKey=" + apiKey).done(function(data){
+// $.ajax("https://hookb.in/b9pdPmJxM1UKGq00Gq19").done(function(data){
 //     console.log(data);
 // })
 
 // .ajax also has a wealth of options we can use in our efforts as programmers:
 // This example: Send out a POST request with some DATA
-$.ajax("https://hookb.in/VGdRZoMaYbSE22bwzXqr",
-    {
-        type: "POST",
-        data: {
-            cohort: "Kalypso",
-            type: "Web Development",
-            yearStarted: 2020
-        }
-    })
+// $.ajax("",
+//     {
+//         type: "POST",
+//         data: {
+//             cohort: "Quasar",
+//             type: "Web Development",
+//             yearStarted: 2021
+//         }
+//     })
 
-// Before we move on: Let's get some CAT FACTS together :)
-// https://alexwohlbruck.github.io/cat-facts/
-//https://cat-fact.herokuapp.com/facts/random < base url
+//TODO: Before we move on: Let's GET a random recipe together :)
+// Help me finish the following AJAX method to console.log the data that comes back!
+// https://www.themealdb.com/ < main website url
+// https://www.themealdb.com/api.php < api documentation main page (hint: looks like there's some kind of URL beneath the 'lookup a single random meal' heading. . and our AJAX method is looking for a url)
 
-// $.ajax("https://cat-fact.herokuapp.com/facts/random").done(function(data){
-//     console.log(data);
-// })
+// $.ajax("url").done(function(data){
+// //do something with data here})
 
 // ~*~ working with responses ~*~
 
@@ -54,21 +54,21 @@ $.ajax("https://hookb.in/VGdRZoMaYbSE22bwzXqr",
 // Asynchronous - be careful presuming your data is coming back with immediacy
 //
 //
-// console.time("catAPI");
-// var randomFact = $.ajax("https://cat-fact.herokuapp.com/facts/random").done(function(data){
-//     console.log(randomFact)
-// });
-// console.timeEnd("catAPI");
+// $(document).ready(function(){
+//     console.time("recipeAPI");
+//     var randomFact = $.ajax("https://www.themealdb.com/api/json/v1/1/random.php").done(function(data){
+//         console.log(randomFact)
+//     });
+//     console.timeEnd("recipeAPI");
+// })
 
 // Different MS = different response times BACK from our API
 
-// console.log("Who wins?");
-
 // .done - when our process is complete [done], do the following [callback function]
 //
-// $.ajax("https://cat-fact.herokuapp.com/facts/random").done(function(data, status, jqXhr){
-//     alert("AJAX call completed successfully!");
-//     console.log("Request status: " + status);
+// $.ajax("https://www.themealdb.com/api/json/v1/1/random.php").done(function(data, status, jqXhr){
+//     alert("AJAX status : " + status);
+//
 //     console.log("Data returned from server:");
 //     console.log(data);
 //     // console.log(jqXhr);
@@ -84,7 +84,7 @@ $.ajax("https://hookb.in/VGdRZoMaYbSE22bwzXqr",
 // .always = always do this stuff (callback function)
 
 // In action:
-// $.ajax("https://cat-fact.herokuapp.com/fatcats/random").done(function(data, status, jqXhr) {
+// $.ajax("https://www.themealdb.com/api/json/v1/1/random.php").done(function(data, status, jqXhr) {
 //     alert("Everything went great! Check out the server's response in the console.");
 //     console.log(data);
 // }).fail(function(jqXhr, status, error) {
@@ -101,41 +101,41 @@ $.ajax("https://hookb.in/VGdRZoMaYbSE22bwzXqr",
 // Putting our ideas into some motion:
 //Our user wants to CLICK A BUTTON and have a RANDOM RECIPE show to them in the view
 //
-// $("#rando").click(function(e){
-//     e.preventDefault();
-//     $.get("https://api.spoonacular.com/recipes/random?apiKey=" + apiKey).done(function(data, status)
+$("#rando").click(function(e){
+    e.preventDefault();
+    $.get("https://www.themealdb.com/api/json/v1/1/random.php").done(function(data, status)
 {
-    // console.log(data);
-    // console.log(data.recipes[0].summary) // Stepping into our data. . data > first level ["recipes"] at the 0 index > the property I want ["summary"] from that data object
+    console.log(data.meals[0]) // Stepping into our data. . data > first level ["meals"] at the 0 index > I can see many object properties to get info from
 
-    // $("#recipeContainer").html("<h1>" + data.recipes[0].title + "</h1>"
-    //     + "<p>" + data.recipes[0].summary + "</p>"
-    //     + "<h4>Instructions</h4>"
-    //     + "<p>" + data.recipes[0].instructions + "</p>")
+    $("#recipeContainer").html("<h1>" + data.meals[0].strMeal + "</h1>"
+        + "<span>" + "Category: " + data.meals[0].strCategory + "</span><br>"
+        + "<img src='" + data.meals[0].strMealThumb + "'>"
+        + "<h4>Instructions</h4>"
+        + "<p>" + data.meals[0].strInstructions + "</p>")
 
-//     })
-// })
+    })
+})
+
 
 //Our user wants to SEARCH IN A FIELD and have the most RELEVANT RECIPE shown to them in the view
 
     $("#userSearch").click(function (e) {
         e.preventDefault();
         let query = $("#searchValue").val(); // a string that was input
-        $.get("https://api.spoonacular.com/recipes/complexSearch?query=" + query + "&apiKey=" + apiKey).done(function (data, status) {
-            // console.log(data.results[0])
-            $("#recipeContainer").html("<h1>" + data.results[0].title + "</h1>"
-                + "<img src='" + data.results[0].image + "'>"
-            )
+        console.log(query);
+        $.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + query).done(function (data, status) {
+            if(data.meals === null){
+                $("#recipeContainer").html("No matches found :/");
+            }
+            var html = "<h1>" + data.meals[0].strMeal + "</h1>"
+                + "<span>" + "Category: " + data.meals[0].strCategory + "</span><br>"
+                + "<img src='" + data.meals[0].strMealThumb + "'>"
+                + "<h4>Instructions</h4>"
+                + "<p>" + data.meals[0].strInstructions + "</p>"
+                + "<a href='" + data.meals[0].strSource + "'>Take me to the recipe</a>"
+            $("#recipeContainer").html(html);
 
-            $.get("https://api.spoonacular.com/recipes/" + data.results[0].id + "/information?apiKey=" + apiKey).done(function (data, status) {
-                console.log(data.summary);
-                $("#recipeContainer").append("<p>" + data.summary + "</p>"
-                    + "<a href='" + data.spoonacularSourceUrl + "'>Check out the recipe!</a>")
-            })
 
         })
-
     })
 
-}
-// https://api.spoonacular.com/recipes/complexSearch?query=lasagna&apiKey=6dd2e7a8653c449da0b7327f582bbd92
